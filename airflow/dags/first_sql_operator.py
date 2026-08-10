79fcc7d052dbe2d826f280a6b2dff1216c42d9c5
Jrodo <trivero@fi.uba.ar>
2026-08-10 20:18:01 -0300

    Setting conection from airflow to a database (in this example case using sqlite) And create a DAG with a SQL operator to create the first table of the database

---
+++ airflow/dags/first_sql_operator.py
@@ -0,0 +1,32 @@
+from airflow.sdk import DAG
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from datetime import datetime,timedelta
+
+default_args = {
+    'owner': 'jrodo',
+    'start_date':datetime(2026,1,1),
+    'depends_on_past': False,
+    'retries': 2,
+    'retry_delay':timedelta(minutes=2)
+}
+with DAG(
+    'first_sql_dag',
+    default_args=default_args,
+    description='use of sql operator for  queries',
+    schedule=None,
+    catchup=False,
+    tags=['sql']
+) as dag:
+    create_table = SQLExecuteQueryOperator(
+        task_id= 'create_table',
+        sql="""
+            CREATE TABLE IF NOT EXISTS users(
+            id INTEGER PRIMARY KEY,
+            name VARCHAR(30) NOT NULL,
+            age INTEGER NOT NULL,
+            is_active BOOL DEFAULT TRUE,
+            created_at TIMESTAMP DEFAUL CURRENT_TIMESTAMP
+            );
+            """,
+            conn_id='my_sqlite_conection'
+    )
